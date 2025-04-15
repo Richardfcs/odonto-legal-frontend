a6fa04b9979c2a13ef9e6070a68ab384fb118dbb
Hadassa <166148227+Hadassa-gomes@users.noreply.github.com>
2025-04-15 18:39:29 +0000

    limpo

--- js/gerenciar_func.js
+++
@@ -1,7 +0,0 @@
-  // Script para mostrar/ocultar o menu dropdown
-  document.querySelectorAll('.fa-pencil-alt').forEach(button => {
-    button.addEventListener('click', function () {
-      const dropdown = this.closest('.relative').querySelector('.dropdown-content');
-      dropdown.classList.toggle('hidden');
-    });
-  });
